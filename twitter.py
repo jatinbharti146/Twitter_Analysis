45e2eed8eb16c566a535b2f9e5dff530ab525459
Jatin Chauhan <47472473+jatinbharti146@users.noreply.github.com>
2020-05-15 22:05:08 +0530

    code uploaded

---
+++ twitter.py
@@ -0,0 +1,69 @@
+from textblob import TextBlob
+import  tweepy
+import matplotlib.pyplot as plt
+
+
+def percentage(part, whole):
+    return 100* float(part)/float(whole)
+
+consumerkey = "SkizqdnNZYw3wtAdDWQgsV4Pg"
+consumerSecret = "ayVoTsNPjU0Z1BgHhkLX6abGFUVM0VQA3FGZ3sFQcMbP2zNexC"
+accessToken = "1191734528403861505-afQrOG7AAryEu2eM2lQOn6zfFSfJmD"
+accessTokenSecret = "Ja1dzhMg5p6ptmrFzA1vvETGKsSOlhlETirOuvEMihZ7o"
+
+auth = tweepy.OAuthHandler(consumer_key=consumerkey, consumer_secret=consumerSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+api = tweepy.API(auth)
+
+searchTerm = input("Enter keyword/hashtag to search about: ")
+noOfSearchTerms = int(input("Enter how many tweets to analyze:"))
+
+tweets = tweepy.Cursor(api.search, q=searchTerm, lan="English").items(noOfSearchTerms)
+
+positive = 0
+negative = 0
+neutral = 0
+polarity = 0
+
+for tweet in (tweets):
+    # print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    polarity += analysis.sentiment.polarity
+
+    if(analysis.sentiment.polarity == 0):
+        neutral += 1
+    elif (analysis.sentiment.polarity < 0.00):
+        negative += 1
+    elif (analysis.sentiment.polarity > 0.00):
+        positive += 1
+
+positive = percentage(positive, noOfSearchTerms)
+negative = percentage(negative, noOfSearchTerms)
+neutral = percentage(neutral, noOfSearchTerms)
+polarity = percentage(polarity, noOfSearchTerms)
+
+positive = format(positive, '.2f')
+neutral = format(neutral, '.2f')
+negative = format(negative, '.2f')
+
+
+print("How people are reacting on " + searchTerm + " by analyzing " + str(noOfSearchTerms) + " Tweets .")
+
+if(polarity == 0):
+    print("Neutral")
+elif(polarity < 0.00):
+    print("Negative")
+elif(polarity > 0.00):
+    print("Positive")
+
+
+    labels = ['Positive ['+str(positive)+'%]', 'Neutral [' + str(neutral) + '%]', 'Negative [' + str(negative) + '%]']
+    sizes = [positive, neutral, negative]
+    colors = ['yellowgreen', 'gold', 'red']
+    patches, texts = plt.pie(sizes, colors=colors, startangle=90)
+    plt.legend(patches, labels, loc="best")
+    plt.title('How people are reacting on '+searchTerm+' by analyzing '+str(noOfSearchTerms)+' Tweets.')
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.show()
+
